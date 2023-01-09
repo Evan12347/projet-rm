3edb0f40911b8b6302909b9d7254ca66ca3a604f
Evan12347 <84842740+Evan12347@users.noreply.github.com>
2023-01-09 15:20:50 +0100

    CSS

--- src/Composants/Favoris/Favoris.js
+++ src/Composants/Favoris/Favoris.js
@@ -1,25 +1,29 @@
 import style from "./Favoris.module.css"
 import {useFavoris} from "../../Hook";
 import {useEffect} from "react";
+import {Link} from "react-router-dom";
 
 export const Favoris = () => {
 
     const {favoris, updateFavoris} = useFavoris();
     useEffect(() => {
-        (async ()=>{
-            const results = await(await fetch(`https://rickandmortyapi.com/api/character/[${favoris.join(",")}]`)).json;
+        (async () => {
+            const results = await (await fetch(`https://rickandmortyapi.com/api/character/[${favoris.join(",")}]`)).json;
         })();
     }, [])
+
     return (
         <div className={style.div}>
             <div className={style.titre}>
                 Favoris
             </div>
-            {favoris.map(favori => (
-                <div key={favori.id}>
-                    {favori.name}
-                </div>
-            ))}
+            <div className={style.liste}>
+                {favoris.map(favori => (
+                    <div key={favori.id} className={style.container}>
+                        <Link to={`/personnage/${favori.id}`}>{favori.name}</Link>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
